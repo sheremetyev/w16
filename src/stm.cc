df541e01dab3563f02917d1b1009a474024773f8
Fyodor Sheremetyev <sheremetyev@gmail.com>
2011-09-15 14:09:25 +0100

    Simplified implementation of cell management in transactions.

--- src/stm.cc
+++ src/stm.cc
@@ -8,21 +8,173 @@
 namespace v8 {
 namespace internal {
 
-class WriteSet {
+// collection of cells, designed with the following objectives:
+// - fast lookup for pointer to owned cell
+// - fast mapping from object address to owned cell pointer
+// - lookup and mapping can be implemented in assembler (in the future)
+// - does not relocate cells (because handles point to the them)
+// - supports updates of object addresses from GC
+//
+// notes:
+// - current implementaion is not optimized yet (N^2 complexity!)
+// - cell can point to the same object (in read set) or to a copy (write set)
+// - original address and mapped cell in read set point to the same object
+// - both original object and our copy are retained in memory by our pointers
+// - all cells are destroyed when transaction ends (no need for handle scopes)
+// - we have to keep own cells in read set because original cells can be
+//   destroyed by handle scope
+
+class CellMap {
  public:
-  void Iterate(ObjectVisitor* v) {
+  CellMap() :
+    first_block_(NULL), last_block_address_(&first_block_) {
   }
 
-  Handle<Object> Get(Handle<Object> obj) {
-    return obj;
+  bool IsMapped(Object** location) {
+    Block* block = first_block_;
+
+    while (block != NULL && block != *last_block_address_) {
+      for (int i = 0; i < BLOCK_SIZE; i++) {
+        if (&block->cells_[i].to_ == location) {
+          return true;
+        }
+      }
+      block = block->next_;
+    }
+
+    if (block != NULL) { // last block
+      for (int i = 0; i < index_; i++) {
+        if (&block->cells_[i].to_ == location) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
-  Handle<Object> Add(Handle<Object> obj, Object* redirect) {
-    return Handle<Object>::null();
+  Object** GetMapping(Object* object) {
+    Block* block = first_block_;
+
+    while (block != NULL && block != *last_block_address_) {
+      for (int i = 0; i < BLOCK_SIZE; i++) {
+        if (block->cells_[i].from_ == object) {
+          return &block->cells_[i].to_;
+        }
+      }
+      block = block->next_;
+    }
+
+    if (block != NULL) { // last block
+      for (int i = 0; i < index_; i++) {
+        if (block->cells_[i].from_ == object) {
+          return &block->cells_[i].to_;
+        }
+      }
+    }
+
+    return NULL;
+  }
+
+  Object** AddMapping(Object* object, Object* redirect) {
+    if (*last_block_address_ == NULL) {
+      *last_block_address_ = (Block*)malloc(sizeof(Block));
+      index_ = 0;
+    }
+
+    // allocate new cell
+    CellPair& pair = (*last_block_address_)->cells_[index_];
+    pair.from_ = object;
+    pair.to_ = redirect;
+    index_++;
+
+    // block is full
+    if (index_ == BLOCK_SIZE) {
+      last_block_address_ = &(*last_block_address_)->next_;
+      *last_block_address_ = NULL;
+    }
+
+    return &pair.to_;
   }
 
   void CommitChanges(Heap* heap) {
-    // heap->CopyBlock(dst_addr, src_addr, dst->Size());
+    Block* block = first_block_;
+
+    while (block != NULL && block != *last_block_address_) {
+      for (int i = 0; i < BLOCK_SIZE; i++) {
+        CellPair& pair = block->cells_[i];
+        ASSERT(pair.from_->IsHeapObject());
+        ASSERT(pair.to_->IsHeapObject());
+        Address dst = reinterpret_cast<HeapObject*>(pair.from_)->address();
+        Address src = reinterpret_cast<HeapObject*>(pair.to_)->address();
+        int size = reinterpret_cast<HeapObject*>(pair.from_)->Size();
+        heap->CopyBlock(dst, src, size);
+      }
+      block = block->next_;
+    }
+
+    if (block != NULL) { // last block
+      for (int i = 0; i < index_; i++) {
+        CellPair& pair = block->cells_[i];
+        ASSERT(pair.from_->IsHeapObject());
+        ASSERT(pair.to_->IsHeapObject());
+        Address dst = reinterpret_cast<HeapObject*>(pair.from_)->address();
+        Address src = reinterpret_cast<HeapObject*>(pair.to_)->address();
+        int size = reinterpret_cast<HeapObject*>(pair.from_)->Size();
+        heap->CopyBlock(dst, src, size);
+      }
+    }
+  }
+
+ private:
+  struct CellPair {
+    Object* from_;
+    Object* to_;
+  };
+
+  const static int BLOCK_SIZE = 100;
+
+  struct Block {
+    CellPair cells_[BLOCK_SIZE];
+    Block* next_;
+  };
+
+  Block*  first_block_;
+  Block** last_block_address_;
+  int index_;
+};
+
+class WriteSet {
+ public:
+  void Iterate(ObjectVisitor* v) {
+    // interate all handles (they may be updated)
+    // update our sets if handles were updated
+  }
+
+  Handle<Object> Get(Handle<Object> obj) {
+    // 1) it is our handle (already redirected)
+    if (map_.IsMapped(obj.location())) {
+      return obj;
+    }
+
+    // 2) we have a cell for the address of a copy of this object
+    Object** location = map_.GetMapping(*obj);
+    if (location != NULL) {
+      return Handle<Object>(location);
+    }
+
+    return Handle<Object>::null();
+  }
+
+  Handle<Object> Add(Handle<Object> obj, Object* redirect) {
+    // create a cell for the redirected object
+    Object** location = map_.AddMapping(*obj, redirect);
+    ASSERT_NOT_NULL(location);
+    return Handle<Object>(location);
+  }
+
+  void CommitChanges(Heap* heap) {
+    map_.CommitChanges(heap);
   }
 
   bool Intersects(const WriteSet& other) {
@@ -30,8 +182,9 @@ class WriteSet {
   }
   
  private:
+  CellMap map_;
 
-   friend class ReadSet;
+  friend class ReadSet;
 };
 
 class ReadSet {
@@ -40,16 +193,33 @@ class ReadSet {
   }
 
   Handle<Object> Get(Handle<Object> obj) {
-    return obj;
+    // 1) it is our handle (already redirected)
+    if (map_.IsMapped(obj.location())) {
+      return obj;
+    }
+
+    // 2) we have our own handle for this object
+    Object** location = map_.GetMapping(*obj);
+    if (location != NULL) {
+      return Handle<Object>(location);
+    }
+
+    return Handle<Object>::null();
   }
 
   Handle<Object> Add(Handle<Object> obj) {
-    return Handle<Object>::null();
+    // create handle pointing to the same object
+    Object** location = map_.AddMapping(*obj, *obj);
+    ASSERT_NOT_NULL(location);
+    return Handle<Object>(location);
   }
 
   bool Intersects(const WriteSet& other) {
     return false;
   }
+
+ private:
+  CellMap map_;
 };
 
 class Transaction {
