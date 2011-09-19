68f0172649c5350d65cb5d207002ce5d1058e9f6
Fyodor Sheremetyev <sheremetyev@gmail.com>
2011-09-19 14:36:31 +0100

    Disable V8 debugger.

--- generate.cmd
+++ generate.cmd
@@ -1,1 +1,1 @@
-python build\gyp_v8 w16\w16.gyp -D target_arch=ia32 -D v8_use_snapshot=false -G msvs_version=2010
+python build\gyp_v8 w16\w16.gyp -D target_arch=ia32 -D v8_use_snapshot=false -D v8_enable_debugger_support=0 -G msvs_version=2010
