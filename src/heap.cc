sheremetyev/w16

commit b242ecd8136e1e0d13c450070214cdba464325b7

Separate storage for thread-local heap roots.

src/heap.cc
@@ -163,10 +163,15 @@ Heap::Heap()
   }
 
   memset(roots_, 0, sizeof(roots_[0]) * kRootListLength);
-  memset(thread_roots_, 0, sizeof(thread_roots_[0]) * kThreadRootListLength);
+  memset(thread_roots_, 0, sizeof(thread_roots_));
   global_contexts_list_ = NULL;
   mark_compact_collector_.heap_ = this;
   external_string_table_.heap_ = this;
+}
+
+
+int Heap::ThreadIndex() {
+  return ThreadId::CurrentInt() - 1;
 }
 
 
@@ -2185,31 +2190,6 @@ bool Heap::CreateInitialObjects() {
   }
   set_prototype_accessors(Foreign::cast(obj));
 
-  // Allocate the code_stubs dictionary. The initial size is set to avoid
-  // expanding the dictionary during bootstrapping.
-  { MaybeObject* maybe_obj = NumberDictionary::Allocate(128);
-    if (!maybe_obj->ToObject(&obj)) return false;
-  }
-  set_code_stubs(NumberDictionary::cast(obj));
-
-  // Allocate the non_monomorphic_cache used in stub-cache.cc. The initial size
-  // is set to avoid expanding the dictionary during bootstrapping.
-  { MaybeObject* maybe_obj = NumberDictionary::Allocate(64);
-    if (!maybe_obj->ToObject(&obj)) return false;
-  }
-  set_non_monomorphic_cache(NumberDictionary::cast(obj));
-
-  { MaybeObject* maybe_obj = AllocatePolymorphicCodeCache();
-    if (!maybe_obj->ToObject(&obj)) return false;
-  }
-  set_polymorphic_code_cache(PolymorphicCodeCache::cast(obj));
-
-  set_instanceof_cache_function(Smi::FromInt(0));
-  set_instanceof_cache_map(Smi::FromInt(0));
-  set_instanceof_cache_answer(Smi::FromInt(0));
-
-  CreateFixedStubs();
-
   // Allocate the dictionary of intrinsic function names.
   { MaybeObject* maybe_obj = StringDictionary::Allocate(Runtime::kNumFunctions);
     if (!maybe_obj->ToObject(&obj)) return false;
@@ -2219,8 +2199,6 @@ bool Heap::CreateInitialObjects() {
     if (!maybe_obj->ToObject(&obj)) return false;
   }
   set_intrinsic_function_names(StringDictionary::cast(obj));
-
-  if (InitializeNumberStringCache()->IsFailure()) return false;
 
   // Allocate cache for single character ASCII strings.
   { MaybeObject* maybe_obj =
@@ -2244,18 +2222,6 @@ bool Heap::CreateInitialObjects() {
 
   // Handling of script id generation is in FACTORY->NewScript.
   set_last_script_id(undefined_value());
-
-  // Initialize keyed lookup cache.
-  isolate_->keyed_lookup_cache()->Clear();
-
-  // Initialize context slot cache.
-  isolate_->context_slot_cache()->Clear();
-
-  // Initialize descriptor cache.
-  isolate_->descriptor_lookup_cache()->Clear();
-
-  // Initialize compilation cache.
-  isolate_->compilation_cache()->Clear();
 
   return true;
 }
@@ -4929,7 +4895,7 @@ void Heap::IterateWeakRoots(ObjectVisitor* v, VisitMode mode) {
 
 void Heap::IterateStrongRoots(ObjectVisitor* v, VisitMode mode) {
   v->VisitPointers(&roots_[0], &roots_[kStrongRootListLength]);
-  v->VisitPointers(&thread_roots_[0], &thread_roots_[kThreadRootListLength]);
+  v->VisitPointers(&thread_roots_[0][0], &thread_roots_[MAX_THREADS][kThreadRootListLength]);
   v->Synchronize("strong_root_list");
 
   v->VisitPointer(BitCast<Object**>(&hidden_symbol_));
@@ -5397,6 +5363,54 @@ bool Heap::Setup(bool create_heap_objects) {
 }
 
 
+bool Heap::ThreadSetup() {
+  SetStackLimits();
+
+  Object* obj;
+
+  // Allocate the code_stubs dictionary. The initial size is set to avoid
+  // expanding the dictionary during bootstrapping.
+  { MaybeObject* maybe_obj = NumberDictionary::Allocate(128);
+    if (!maybe_obj->ToObject(&obj)) return false;
+  }
+  set_code_stubs(NumberDictionary::cast(obj));
+
+  // Allocate the non_monomorphic_cache used in stub-cache.cc. The initial size
+  // is set to avoid expanding the dictionary during bootstrapping.
+  { MaybeObject* maybe_obj = NumberDictionary::Allocate(64);
+    if (!maybe_obj->ToObject(&obj)) return false;
+  }
+  set_non_monomorphic_cache(NumberDictionary::cast(obj));
+
+  { MaybeObject* maybe_obj = AllocatePolymorphicCodeCache();
+    if (!maybe_obj->ToObject(&obj)) return false;
+  }
+  set_polymorphic_code_cache(PolymorphicCodeCache::cast(obj));
+
+  set_instanceof_cache_function(Smi::FromInt(0));
+  set_instanceof_cache_map(Smi::FromInt(0));
+  set_instanceof_cache_answer(Smi::FromInt(0));
+
+  CreateFixedStubs();
+
+  if (InitializeNumberStringCache()->IsFailure()) return false;
+
+  // Initialize keyed lookup cache.
+  isolate_->keyed_lookup_cache()->Clear();
+
+  // Initialize context slot cache.
+  isolate_->context_slot_cache()->Clear();
+
+  // Initialize descriptor cache.
+  isolate_->descriptor_lookup_cache()->Clear();
+
+  // Initialize compilation cache.
+  isolate_->compilation_cache()->Clear();
+
+  return true;
+}
+
+
 void Heap::SetStackLimits() {
   ASSERT(isolate_ != NULL);
   ASSERT(isolate_ == isolate());
@@ -5405,10 +5419,10 @@ void Heap::SetStackLimits() {
 
   // Set up the special root array entries containing the stack limits.
   // These are actually addresses, but the tag makes the GC ignore it.
-  thread_roots_[kStackLimitRootIndex] =
+  thread_roots()[kStackLimitRootIndex] =
       reinterpret_cast<Object*>(
           (isolate_->stack_guard()->jslimit() & ~kSmiTagMask) | kSmiTag);
-  thread_roots_[kRealStackLimitRootIndex] =
+  thread_roots()[kRealStackLimitRootIndex] =
       reinterpret_cast<Object*>(
           (isolate_->stack_guard()->real_jslimit() & ~kSmiTagMask) | kSmiTag);
 }

src/heap.h
@@ -337,6 +337,7 @@ class Heap {
   // also creates the basic non-mutable objects.
   // Returns whether it succeeded.
   bool Setup(bool create_heap_objects);
+  bool ThreadSetup();
 
   // Destroys all memory allocated by the heap.
   void TearDown();
@@ -941,10 +942,10 @@ class Heap {
   // You can't use type::cast during GC because the assert fails.
 #define ROOT_ACCESSOR(type, name, camel_name)                                  \
   type* name() {                                                               \
-    return type::cast(thread_roots_[k##camel_name##RootIndex]);                       \
+    return type::cast(thread_roots()[k##camel_name##RootIndex]);               \
   }                                                                            \
   type* raw_unchecked_##name() {                                               \
-    return reinterpret_cast<type*>(thread_roots_[k##camel_name##RootIndex]);          \
+    return reinterpret_cast<type*>(thread_roots()[k##camel_name##RootIndex]);  \
   }
   THREAD_ROOT_LIST(ROOT_ACCESSOR)
 #undef ROOT_ACCESSOR
@@ -1052,7 +1053,7 @@ class Heap {
 
   // Sets the stub_cache_ (only used when expanding the dictionary).
   void public_set_code_stubs(NumberDictionary* value) {
-    thread_roots_[kCodeStubsRootIndex] = value;
+    thread_roots()[kCodeStubsRootIndex] = value;
   }
 
   // Support for computing object sizes for old objects during GCs. Returns
@@ -1064,7 +1065,7 @@ class Heap {
 
   // Sets the non_monomorphic_cache_ (only used when expanding the dictionary).
   void public_set_non_monomorphic_cache(NumberDictionary* value) {
-    thread_roots_[kNonMonomorphicCacheRootIndex] = value;
+    thread_roots()[kNonMonomorphicCacheRootIndex] = value;
   }
 
   void public_set_empty_script(Script* script) {
@@ -1076,7 +1077,8 @@ class Heap {
 
   // Generated code can embed this address to get access to the roots.
   Object** roots_address() { return roots_; }
-  Object** thread_roots_address() { return thread_roots_; }
+  Object** thread_roots_address() { return thread_roots_[ThreadIndex()]; }
+  Object** thread_roots() { return thread_roots_address(); }
 
   // Get address of global contexts list for serialization support.
   Object** global_contexts_list_address() {
@@ -1326,6 +1328,8 @@ class Heap {
  private:
   Heap();
 
+  static int ThreadIndex();
+
   // This can be calculated directly from a pointer to the heap; however, it is
   // more expedient to get at the isolate directly from within Heap methods.
   Isolate* isolate_;
@@ -1388,7 +1392,7 @@ class Heap {
 
 #define ROOT_ACCESSOR(type, name, camel_name)                                  \
   inline void set_##name(type* value) {                                 \
-    thread_roots_[k##camel_name##RootIndex] = value;                                  \
+    thread_roots()[k##camel_name##RootIndex] = value;                                  \
   }
   THREAD_ROOT_LIST(ROOT_ACCESSOR)
 #undef ROOT_ACCESSOR
@@ -1434,7 +1438,7 @@ class Heap {
   int old_gen_exhausted_;
 
   Object* roots_[kRootListLength];
-  Object* thread_roots_[kThreadRootListLength];
+  Object* thread_roots_[MAX_THREADS][kThreadRootListLength];
 
   Object* global_contexts_list_;
 

src/isolate.cc
@@ -1600,10 +1600,6 @@ bool Isolate::Init(Deserializer* des) {
   memory_allocator_ = new MemoryAllocator(this);
   code_range_ = new CodeRange(this);
 
-  // Safe after setting Heap::isolate_, initializing StackGuard and
-  // ensuring that Isolate::Current() == this.
-  heap_.SetStackLimits();
-
   bootstrapper_ = new Bootstrapper();
 
   // Enable logging before setting up the heap
@@ -1625,6 +1621,20 @@ bool Isolate::Init(Deserializer* des) {
   if (!heap_.Setup(create_heap_objects)) {
     V8::SetFatalError();
     return false;
+  }
+
+  { int current_thread = Thread::GetThreadLocalInt(thread_id_key_);
+    void* current_top = Thread::GetThreadLocal(thread_local_top_key_);
+    for (int i = 0; i < MAX_THREADS; i++) {
+      Thread::SetThreadLocalInt(thread_id_key_, i + 1);
+      Thread::SetThreadLocal(thread_local_top_key_, tops_[i]);
+      if (!heap_.ThreadSetup()) {
+        V8::SetFatalError();
+        return false;
+      }
+    }
+    Thread::SetThreadLocalInt(thread_id_key_, current_thread);
+    Thread::SetThreadLocal(thread_local_top_key_, current_top);
   }
 
   InitializeThreadLocal();

commit 3fc95e11d9f201cbf771edc5c6f6429f898807a7

Fix thread-local roots iteration.
Bug was introduced in commit b242ecd "Separate storage for thread-local heap roots".

src/heap.cc
@@ -4895,7 +4895,9 @@ void Heap::IterateWeakRoots(ObjectVisitor* v, VisitMode mode) {
 
 void Heap::IterateStrongRoots(ObjectVisitor* v, VisitMode mode) {
   v->VisitPointers(&roots_[0], &roots_[kStrongRootListLength]);
-  v->VisitPointers(&thread_roots_[0][0], &thread_roots_[MAX_THREADS][kThreadRootListLength]);
+  for (int i = 0; i < MAX_THREADS; i++) {
+    v->VisitPointers(&thread_roots_[i][0], &thread_roots_[i][kThreadRootListLength]);
+  }
   v->Synchronize("strong_root_list");
 
   v->VisitPointer(BitCast<Object**>(&hidden_symbol_));